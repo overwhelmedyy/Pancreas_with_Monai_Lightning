aca17bed040b1d134dee67d6f9699eca5f7e8715
yy4551 <100992443+yy4551@users.noreply.github.com>
2024-01-21 13:28:23 +0800

    Initial commit

---
+++ try.py
@@ -0,0 +1,270 @@
+import lightning
+
+from monai.utils import set_determinism
+from monai.transforms import (
+    AsDiscrete,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    ScaleIntensityRanged,
+    Spacingd,
+    EnsureType,
+    LabelToMask
+)
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.metrics import DiceMetric
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai.data import list_data_collate, decollate_batch, DataLoader, PersistentDataset
+import matplotlib.pyplot as plt
+import tempfile
+import shutil
+import torch
+import os
+import glob
+from lightning.pytorch.loggers import TensorBoardLogger
+
+directory = os.environ.get("MONAI_DATA_DIRECTORY")
+data_dir = os.path.join(directory, "Task09_Spleen")
+persistent_cache = os.path.join(data_dir, "persistent_cache")
+tensorboard_dir = r"./runs"
+cuda = torch.device("cuda:0")
+
+set_determinism(seed=0)
+
+tensorboard_logger = TensorBoardLogger(tensorboard_dir)
+
+class PancreasDataModule(lightning.LightningDataModule):
+    def __init__(self):
+        super().__init__()
+
+
+class Net(lightning.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self._model = UNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=2,
+            channels=(16, 32, 64, 128, 256),
+            strides=(2, 2, 2, 2),
+            num_res_units=2,
+            norm=Norm.BATCH,
+        )
+
+        self.loss_function = DiceLoss(to_onehot_y=True, softmax=True)
+        self.post_pred = Compose([EnsureType("tensor", device=torch.device("cpu")), AsDiscrete(argmax=True, to_onehot=2)])
+        self.post_label = Compose([EnsureType("tensor", device=torch.device("cpu")), AsDiscrete(to_onehot=2)])
+        self.dice_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=False)
+        self.best_val_dice = 0
+        self.best_val_epoch = 0
+        self.validation_step_outputs = []
+        self.tb_logger = tensorboard_logger.experiment
+        # log1:画出网络图
+        self.tb_logger.add_graph(self._model, torch.rand(1, 1, 96, 96, 96), verbose=True)
+
+    def forward(self, x):
+        return self._model(x)
+
+    def prepare_data(self):
+        # set up the correct data path
+        train_images = sorted(glob.glob(os.path.join(data_dir, "imagesTr", "*.nii.gz")))
+        train_labels = sorted(glob.glob(os.path.join(data_dir, "labelsTr", "*.nii.gz")))
+        data_dicts = [
+            {"image": image_name, "label": label_name} for image_name, label_name in zip(train_images, train_labels)
+        ]  # 注意到data_dicts是一个数组
+        train_files, val_files = data_dicts[:-9], data_dicts[-9:]
+
+        # set deterministic training for reproducibility
+        set_determinism(seed=0)
+
+        # define the data transforms
+        train_transforms = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                EnsureChannelFirstd(keys=["image", "label"]),
+                Orientationd(keys=["image", "label"], axcodes="RAS"),
+                Spacingd(
+                    keys=["image", "label"],
+                    pixdim=(1.5, 1.5, 2.0),
+                    mode=("bilinear", "nearest"),
+                ),
+                ScaleIntensityRanged(
+                    keys=["image"],
+                    a_min=-57,
+                    a_max=164,
+                    b_min=0.0,
+                    b_max=1.0,
+                    clip=True,
+                ),
+                CropForegroundd(keys=["image", "label"], source_key="image"),
+                # randomly crop out patch samples from
+                # big image based on pos / neg ratio
+                # the image centers of negative samples
+                # must be in valid image area
+                RandCropByPosNegLabeld(
+                    keys=["image", "label"],
+                    label_key="label",
+                    spatial_size=(96, 96, 96),
+                    pos=1,
+                    neg=1,
+                    num_samples=4,
+                    image_key="image",
+                    image_threshold=0,
+                ),
+                # user can also add other random transforms
+                #                 RandAffined(
+                #                     keys=['image', 'label'],
+                #                     mode=('bilinear', 'nearest'),
+                #                     prob=1.0,
+                #                     spatial_size=(96, 96, 96),
+                #                     rotate_range=(0, 0, np.pi/15),
+                #                     scale_range=(0.1, 0.1, 0.1)),
+            ]
+        )
+        val_transforms = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                EnsureChannelFirstd(keys=["image", "label"]),
+                Orientationd(keys=["image", "label"], axcodes="RAS"),
+                Spacingd(
+                    keys=["image", "label"],
+                    pixdim=(1.5, 1.5, 2.0),
+                    mode=("bilinear", "nearest"),
+                ),
+                ScaleIntensityRanged(
+                    keys=["image"],
+                    a_min=-57,
+                    a_max=164,
+                    b_min=0.0,
+                    b_max=1.0,
+                    clip=True,
+                ),
+                CropForegroundd(keys=["image", "label"], source_key="image"),
+            ]
+        )
+
+        # we use cached datasets - these are 10x faster than regular datasets
+        self.train_ds = PersistentDataset(
+            data=train_files,
+            transform=train_transforms,
+            cache_dir=persistent_cache,
+        )
+        self.val_ds = PersistentDataset(
+            data=val_files,
+            transform=val_transforms,
+            cache_dir=persistent_cache,
+        )
+
+    #         self.train_ds = monai.data.Dataset(
+    #             data=train_files, transform=train_transforms)
+    #         self.val_ds = monai.data.Dataset(
+    #             data=val_files, transform=val_transforms)
+
+    def train_dataloader(self):
+        train_loader = DataLoader(
+            self.train_ds,
+            batch_size=2,
+            shuffle=True,
+            num_workers=4,
+            persistent_workers=True,
+            collate_fn=list_data_collate, #把每个batch的list of data合并到一个list中
+        )
+        return train_loader
+
+    def val_dataloader(self):
+        val_loader = DataLoader(
+            self.val_ds,
+            batch_size=1,
+            num_workers=4,
+            persistent_workers=True
+        )
+        return val_loader
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self._model.parameters(), 5e-3)
+        return optimizer
+
+
+    def training_step(self, batch, batch_idx):
+        images, labels = batch["image"], batch["label"]
+        output = self.forward(images)
+        loss = self.loss_function(output, labels)
+        tensorboard_logs = {"train_loss": loss.item()}
+        self.log_dict(tensorboard_logs,prog_bar=True,logger=True,on_step=True)
+        return {"loss": loss, "log": tensorboard_logs}
+
+    # def on_train_epoch_end(self):
+    #     for name,para in self.named_parameters():
+    #         self.tb_logger.add_histogram(name,para,self.current_epoch)
+
+
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch["image"], batch["label"]
+        self.tb_logger.add_text("label shape",f"{labels[0].shape}")
+        roi_size = (160, 160, 160)
+        sw_batch_size = 4
+        outputs = sliding_window_inference(images, roi_size, sw_batch_size, self.forward)
+        loss = self.loss_function(outputs, labels)
+        outputs = [self.post_pred(i) for i in decollate_batch(outputs)]
+        labels = [self.post_label(i) for i in decollate_batch(labels)]
+        self.dice_metric(y_pred=outputs, y=labels)
+        d = {"val_loss": loss, "val_number": len(outputs)}
+        self.validation_step_outputs.append(d)
+        self.log_dict(d,logger=True)
+
+        return d
+
+    def on_validation_epoch_end(self):
+        val_loss, num_items = 0, 0
+        for output in self.validation_step_outputs:
+            val_loss += output["val_loss"].sum().item()
+            num_items += output["val_number"]
+        mean_val_dice = self.dice_metric.aggregate().item()
+        self.dice_metric.reset()
+        mean_val_loss = torch.tensor(val_loss / num_items)
+        tensorboard_logs = {
+            "val_dice": mean_val_dice,
+            "val_loss": mean_val_loss,
+        }
+        if mean_val_dice > self.best_val_dice:
+            self.best_val_dice = mean_val_dice
+            self.best_val_epoch = self.current_epoch
+        print(
+            f"current epoch: {self.current_epoch} "
+            f"current mean dice: {mean_val_dice:.4f}"
+            f"\nbest mean dice: {self.best_val_dice:.4f} "
+            f"at epoch: {self.best_val_epoch}"
+        )
+        self.validation_step_outputs.clear()
+        self.log_dict(tensorboard_logs,logger=True)
+
+
+        return {"log": tensorboard_logs}
+
+
+
+if __name__ == "__main__":
+    net = Net()
+
+    log_dir = os.path.join(data_dir, "logs")
+    tb_logger = TensorBoardLogger(log_dir)
+
+    trainer = lightning.Trainer(
+        devices="auto",
+        max_epochs=100,
+        logger=tb_logger,
+        log_every_n_steps=1,
+        enable_checkpointing=True,
+        deterministic=True,
+        check_val_every_n_epoch=None
+    )
+
+    trainer.fit(net)
+    print(f"train completed, best_metric: {net.best_val_dice:.4f} " f"at epoch {net.best_val_epoch}")
+
+
